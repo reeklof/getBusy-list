7292850ef184c44a36c644f104591ad6e784d7cd
reeklof <reeklof@live.com>
2019-03-05 09:10:46 +0100

    added small changes

--- src/views/NewTodo.vue
+++ src/views/NewTodo.vue
@@ -1,81 +1,78 @@
 <template>
-    <main id="new-todo">
-        <header>
-
-        </header>
-        <section class="content">
-            <input type="text" v-model="newTodo">
-        </section>
-        <footer>
-            <p>{{ newTodo.length }} / 30 tecken</p>
-            <a href="#" class="btn" v-on:click="addTodo">Add a new Busy Thing</a>
-        </footer>
-    </main>
+  <main id="new-todo">
+    <header></header>
+    <section class="content">
+      <input type="text" v-model="newTodo" @keydown.enter="addTodo">
+    </section>
+    <footer>
+      <p>{{ newTodo.length }} / 30 tecken</p>
+      <a href="#" class="btn" @click="addTodo">Add a new Busy Thing</a>
+    </footer>
+  </main>
 </template>
 
 <script>
 export default {
-    name: "newTodo",
-    data() {
-        return {
-            newTodo: ''
-        }
-    },
-    methods: {
-        addTodo(){
-            let todo = {
-                done: false,
-                text: this.newTodo
-            }
-            this.$store.dispatch('newTodo', todo);
-            this.newTodo = '';
-            this.$store.commit('swipe', 0);
-        }
+  name: "newTodo",
+  data() {
+    return {
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      let todo = {
+        done: false,
+        text: this.newTodo
+      };
+      this.$store.dispatch("newTodo", todo);
+      this.newTodo = "";
+      this.$store.commit("swipe", 0);
     }
-}
+  }
+};
 </script>
 
 <style lang='scss'>
-
 #new-todo {
-    background: rgb(93, 176, 93); 
+  background: rgb(93, 176, 93);
 
-    .content {
-        display: flex; 
-        justify-content: center; 
-        align-items: center; 
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-        input {
-            font-size: 1.4rem;
-            font-weight: 700;
-            border: none; 
-            appearance: none;
-            background: none;
-            border: 1px solid white;
-            text-align: center; 
-            padding: .25rem;
+    input {
+      font-size: 1.4rem;
+      font-weight: 700;
+      border: none;
+      appearance: none;
+      background: none;
+      border: 1px solid white;
+      text-align: center;
+      padding: 0.25rem;
 
-            &:focus {
-                outline: none;
-            }
-        }
+      &:focus {
+        outline: none;
+      }
     }
-    footer {
-        display: flex; 
-        flex-direction: column; 
-        justify-content: flex-end; 
+  }
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
 
-        a.btn {
-            display: flex;
-            justify-content: center; 
-            align-items: center; 
-            background: #222;
-            color: white;
-            font-weight: 700;
-            text-decoration: none; 
-            height: 3rem;
-            border-radius: 99rem;
-        }
+    a.btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #222;
+      color: white;
+      font-weight: 700;
+      text-decoration: none;
+      height: 3rem;
+      border-radius: 99rem;
     }
+  }
 }
 </style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,73 +1,70 @@
 <template>
-    <main id="todos">
-        <header>
-            <h1>Get Busy</h1>
-            <p>get things done...</p>
-        </header>
-        <section class="content">
-            <todo v-for="(todoData, index) in todos" :key="index" :index="index" 
-            :todo="todoData"/>
-        </section>
-        <footer>
-            <a href="#" class="btn" v-touch:swipe.left="swipe">Swipe to add new</a>
-        </footer>
-    </main>
+  <main id="todos">
+    <header>
+      <h1>Get Busy</h1>
+      <p>get things done...</p>
+    </header>
+    <section class="content">
+      <todo v-for="(todoData, index) in todos" :key="index" :index="index" :todo="todoData"/>
+    </section>
+    <footer>
+      <a href="#" class="btn" @click="swipe">Click to add new</a>
+    </footer>
+  </main>
 </template>
 
 <script>
-import todo from '@/components/Todo';
+import todo from "@/components/Todo";
 
 export default {
-    name: "todos",
-    computed: {
-        todos() {
-            return this.$store.getters.todos;
-        }
-    },
-    methods: {
-        swipe() {
-            this.$store.commit('swipe', 1);
-        }
-    },
-    components: {
-        todo
+  name: "todos",
+  computed: {
+    todos() {
+      return this.$store.getters.todos;
     }
-}
+  },
+  methods: {
+    swipe() {
+      this.$store.commit("swipe", 1);
+    }
+  },
+  components: {
+    todo
+  }
+};
 </script>
 
 <style lang='scss'>
-#todos{
-    background: rgb(245, 171, 142); 
+#todos {
+  background: rgb(245, 171, 142);
 
-    header {
-
-        h1 {
-            margin: 0;
-            padding: 0;
-            font-weight: 700;
-        }
-        p {
-            margin: 0;
-            font-style: italic;
-        }
+  header {
+    h1 {
+      margin: 0;
+      padding: 0;
+      font-weight: 700;
     }
-    footer {
-        display: flex; 
-        flex-direction: column; 
-        justify-content: flex-end; 
-
-        a.btn {
-            display: flex;
-            justify-content: center; 
-            align-items: center; 
-            background: #222;
-            color: white;
-            font-weight: 700;
-            text-decoration: none; 
-            height: 3rem;
-            border-radius: 99rem;
-        }
+    p {
+      margin: 0;
+      font-style: italic;
     }
+  }
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    a.btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: #222;
+      color: white;
+      font-weight: 700;
+      text-decoration: none;
+      height: 3rem;
+      border-radius: 99rem;
+    }
+  }
 }
-
 </style>
